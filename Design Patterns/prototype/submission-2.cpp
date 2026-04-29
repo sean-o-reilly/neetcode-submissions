8988375d53757d8d1f32bae8ea0dd5d2ea3e65b5
Sean OReilly <oreilly.e.sean@gmail.com>
2026-04-29 13:52:12 -0500

    Add: prototype - submission-2

---
+++ Design Patterns/prototype/submission-2.cpp
@@ -0,0 +1,54 @@
+#include <ranges>
+
+class Shape {
+public:
+    virtual ~Shape() {}
+    virtual Shape* clone() const = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) : width(w), height(h) {}
+
+    int getWidth() const {
+        return width;
+    }
+
+    int getHeight() const {
+        return height;
+    }
+
+    Shape* clone() const override {
+        return new Rectangle{width, height};
+    }
+};
+
+class Square : public Shape {
+private:
+    int length;
+
+public:
+    Square(int l) : length(l) {}
+
+    int getLength() const {
+        return length;
+    }
+
+    Shape* clone() const override {
+        return new Square{length};
+    }
+};
+
+class Test {
+public:
+    vector<Shape*> cloneShapes(const vector<Shape*>& shapes) {
+        return
+            shapes |
+            std::views::transform([](Shape* shape){return shape->clone();}) |
+            std::ranges::to<std::vector>();
+    }
+};
