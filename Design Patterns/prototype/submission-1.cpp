0fa1f0e6ab2a30b7c30b24d780f43d684a44a1bd
Sean OReilly <oreilly.e.sean@gmail.com>
2026-04-29 13:43:48 -0500

    Add: prototype - submission-1

---
+++ Design Patterns/prototype/submission-1.cpp
@@ -0,0 +1,57 @@
+class Shape {
+public:
+    virtual ~Shape() {}
+    virtual Shape* clone() const = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) : width(w), height(h) {}
+
+    int getWidth() const {
+        return width;
+    }
+
+    int getHeight() const {
+        return height;
+    }
+
+    Shape* clone() const override {
+        return new Rectangle{width, height};
+    }
+};
+
+class Square : public Shape {
+private:
+    int length;
+
+public:
+    Square(int l) : length(l) {}
+
+    int getLength() const {
+        return length;
+    }
+
+    Shape* clone() const override {
+        return new Square{length};
+    }
+};
+
+class Test {
+public:
+    vector<Shape*> cloneShapes(const vector<Shape*>& shapes) {
+        vector<Shape*> res;
+        res.reserve(shapes.size());
+
+        std::transform(shapes.begin(), shapes.end(), std::back_inserter(res),
+            [](Shape* shape){
+            return shape->clone();
+        });
+
+        return res;
+    }
+};
