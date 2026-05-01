7b20bc5d8965ab3a379ef5098b3bb15db91a4fb9
Sean OReilly <oreilly.e.sean@gmail.com>
2026-04-30 23:41:16 -0500

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        std::istringstream iss{std::move(s)};
+        while (iss)
+            iss >> s;
+        
+        return s.length();
+    }
+};
