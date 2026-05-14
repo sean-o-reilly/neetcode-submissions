931ac7e9147b35db1c070459c47002a6daa9fcc6
Sean OReilly <oreilly.e.sean@gmail.com>
2026-05-14 01:22:27 -0500

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    { 
+        std::vector<std::unordered_set<int>> buckets(nums.size() + 1);
+        std::unordered_map<int, int> hm;
+
+        for (auto num : nums)
+            hm[num]++;
+
+        for (auto [k, v] : hm)
+            buckets.at(buckets.size() - v).insert(k);
+
+        std::vector<int> res;
+
+        for (const auto& bucket : buckets)
+        {
+            for (auto num : bucket)
+            {
+                res.push_back(num);
+                if (res.size() == k)
+                    return res;
+            }
+        }
+
+        throw std::logic_error("not enough top nums?");
+    }
+};
+
+
+/*
+bucket sort
+[1, 2, 2, 3, 3, 3]
+        [{}, {1}, {2}, {3}, {}, {}, {}]
+times = 0    1    2   3  4   5    6
+
+       [{}, {}, {7}, {}, {}, {}, {}, {}]
+times =  0   1   2   3   4   5  6    7
+
+*/
