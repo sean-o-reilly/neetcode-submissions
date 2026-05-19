02ebeb8f7f314feda584142e7fb9ce77df775d62
Sean OReilly <oreilly.e.sean@gmail.com>
2026-05-19 13:37:27 -0500

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2)
+    {
+        int left = 0;
+        int right = 0;
+        std::string res;
+        res.reserve(word1.length() + word2.length());
+
+        while (left < word1.length() || right < word2.length())    
+        {
+            if (left < word1.length())
+            {
+                res += word1.at(left);
+                ++left;
+            }
+
+            if (right < word2.length())
+            {
+                res += word2.at(right);
+                ++right;
+            }
+        }
+
+        return res;
+    }
+};
