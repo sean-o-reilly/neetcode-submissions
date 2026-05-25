6bb48dfffd637eaf071bd6cced05a975ee7d402f
Sean OReilly <oreilly.e.sean@gmail.com>
2026-05-25 16:12:14 -0500

    Add: reverse-linked-list-ii - submission-0

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-0.cpp
@@ -0,0 +1,120 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right)
+    {
+        int pos = 1;
+        ListNode* curr = head;
+        ListNode* leftBound = nullptr;
+
+        while (pos < left)
+        {
+            leftBound = curr;
+            curr = curr->next;
+            ++pos;
+        }
+
+        ListNode* leftPtr = curr;
+
+        while (pos < right)
+        {
+            curr = curr->next;
+            ++pos;
+        }
+
+        ListNode* rightPtr = curr;
+        ListNode* rightBound = rightPtr->next;
+
+        std::cerr << "Reversing between " << leftPtr->val << " and " << rightPtr->val << std::endl;
+
+        curr = leftPtr;
+        ListNode* prev = nullptr;
+
+        while (curr != rightBound)
+        {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        leftPtr->next = rightBound;
+        ListNode* reverseHead = rightPtr;
+
+        if (leftBound)
+        {
+            leftBound->next = reverseHead;
+            return head;
+        }
+        else
+        {
+            return reverseHead;
+        }
+
+        throw std::logic_error("?");
+    }
+};
+
+
+/*
+
+0-1-2-3-4-5
+left = 3
+right = 4
+reverse 2-3
+0-1-3-2-4-5
+
+- position pointers
+- reverse list within window
+- repair list
+
+pos = 1
+curr = head
+leftBound = null
+
+while pos < left
+    leftBound = curr
+    curr = curr->next
+    ++pos
+left = curr
+
+while pos < right
+    curr = curr->next
+    ++pos
+right = curr
+rightBound = right next // after reversing, set as head's next
+
+
+// reverse starting at left
+curr = left
+prev = curr
+
+while curr != rightBound
+    temp = curr->next
+    curr->next = prev
+    curr = temp
+    // or curr = std::exchange(curr->next, prev)
+
+// repair and return
+
+left->next = rightBound
+
+reverseHead = right
+    if leftBound non null,
+        leftBound next = reverseHead
+        return head
+    else
+        return reverseHead
+    
+
+
+*/
