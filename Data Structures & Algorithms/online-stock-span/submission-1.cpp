c6598f00ecb9905ee880e3d1fe1a460ee485b937
Sean OReilly <oreilly.e.sean@gmail.com>
2026-05-25 15:25:48 -0500

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.cpp
@@ -0,0 +1,61 @@
+class StockSpanner {
+public:
+    StockSpanner() = default;
+    
+    int next(int price)
+    {
+        while (!st.empty() && st.top().first <= price)
+        {
+            st.pop();
+        }
+
+        if (st.empty())
+        {
+            st.push({price, currDay++});
+            return currDay;
+        }
+
+        int res = currDay - st.top().second;
+        st.push({price, currDay++});
+        return res;
+    }
+
+    using Pair = std::pair<int, int>;
+    std::stack<Pair> st{};
+    int currDay{};
+};
+
+/*
+[100, 80, 60, 70, 60, 75, 85]
+
+stack
+price, day 
+
+int next(int currPrice)
+{
+    print err
+
+    while stack non empty, and stack top.price <= currPrice
+        stack pop
+    
+    if stack empty
+        stack push {currPrice, currDay++}
+        return currDay
+    else
+        int res = currDay - stack top day
+        stack push 
+
+        ++currDay
+        return res
+}
+
+pop stack until top > curr price
+return curr day - top of stack day
+push curr price, curr day
+
+999, 7
+30, 6
+40, 2
+50, 1
+60, 0
+*/
